7199303399f4401bef4b43b1d54ed7920f773acc
Not_Enough <superlexlive@gmail.com>
2024-11-30 13:51:57 +0300

    module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,39 +1,89 @@
-class Horse:
-    def __init__(self):
-        self.x_distance = 0
-        self.sound = 'Frrr'
+import random
 
-    def run(self, dx):
-        self.x_distance += dx
+class Animal:
+    def __init__(self, speed):
+        self.live = True
+        self.sound = None
+        self._DEGREE_OF_DANGER = 0
+        self._cords = [0, 0, 0]
+        self.speed = speed
 
-class Eagle:
-    def __init__(self):
-        self.y_distance = 0
-        self.sound = 'I train, eat, sleep, and repeat'
+    def move(self, dx, dy, dz):
+        """Изменяет координаты с учётом скорости"""
+        if self._cords[2] + dz < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._cords[0] += dx * self.speed
+            self._cords[1] += dy * self.speed
+            self._cords[2] += dz * self.speed
 
-    def fly(self, dy):
-        self.y_distance += dy
+    def get_cords(self):
+        """Выводит координаты"""
+        return f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}"
 
-class Pegasus(Eagle, Horse):
-    def __init__(self):
-        super().__init__()
-        Horse.__init__(self)
-        self.sound = 'I train, eat, sleep, and repeat'
+    def attack(self):
+        """Выводит сообщение в зависимости от степени опасности"""
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
 
-    def move(self, dx, dy):
-        Horse.run(self, dx)
-        Eagle.fly(self, dy)
+    def speak(self):
+        """Выводит звук животного"""
+        print(self.sound if self.sound else "No sound")
 
-    def get_pos(self):
-        return self.x_distance, self.y_distance
 
-    def voice(self):
-        print(self.sound)
+class Bird(Animal):
+    def __init__(self, speed):
+        super().__init__(speed)
+        self.beak = True
+        self._DEGREE_OF_DANGER = 1
 
-p1 = Pegasus()
-print(p1.get_pos())
-p1.move(10, 15)
-print(p1.get_pos())
-p1.move(-5, 20)
-print(p1.get_pos())
-p1.voice()
+    def lay_eggs(self):
+        """Выводит количество яиц"""
+        eggs = random.randint(1, 4)
+        print(f"Here are(is) {eggs} eggs for you")
+
+
+class AquaticAnimal(Animal):
+    def __init__(self, speed):
+        super().__init__(speed)
+        self._DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        """Метод для ныряния, уменьшает координату Z"""
+        dz = abs(dz)
+        self._cords[2] -= dz
+        self.speed /= 2
+
+
+class PoisonousAnimal(Animal):
+    def __init__(self, speed):
+        super().__init__(speed)
+        self._DEGREE_OF_DANGER = 8
+
+
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+    def __init__(self, speed):
+        Bird.__init__(self, speed)
+        AquaticAnimal.__init__(self, speed)
+        PoisonousAnimal.__init__(self, speed)
+        self.sound = "Click-click-click"
+
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+
+db.attack()
+
+db.move(1, 2, 3)
+print(db.get_cords())
+
+db.dive_in(6)
+print(db.get_cords())
+
+db.lay_eggs()
